df89f560e3c348f9907a97e4fa0c26ef6982ef34
Alex Jercan <jercan_alex27@yahoo.com>
2026-04-05 10:34:15 +0300

    fix: add overlay for macros

--- flake.nix
+++ flake.nix
@@ -1,19 +1,24 @@
 {
   description = "A Neovim plugin and CLI tool for tracking food macros";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
-        # Optional: use external flake logic, e.g.
-        # inputs.foo.flakeModules.default
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
       ];
-      flake = {
-        # Put your original flake attributes here.
-      };
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
         # self',
@@ -35,13 +40,13 @@
 
           installPhase = ''
             mkdir -p $out/bin $out/share/macros
-            
+
             # Copy the lua modules
             cp -r lua $out/share/macros/
-            
+
             # Copy the CLI script
             cp macros.lua $out/share/macros/
-            
+
             # Create a wrapper script that sets up the correct paths
             makeWrapper ${pkgs.lua}/bin/lua $out/bin/macros \
               --add-flags "$out/share/macros/macros.lua"
@@ -54,12 +59,21 @@
           };
         };
 
-        devShells.default = pkgs.mkShell rec {
+        devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             lua
             luajitPackages.luacheck
             stylua
           ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+        overlays.default = final: prev: {
+          macros = self.packages.${final.stdenv.hostPlatform.system}.default;
         };
       };
     };
